73690d754ac30610cb7d47f4e6467e070ce9d8cf
serendipity <jiang040906@gmail.com>
2024-11-29 10:46:19 +0800

    Error message prompts are optimized

--- internal/biz/errors.go
+++ internal/biz/errors.go
@@ -3,11 +3,11 @@ package biz
 import "errors"
 
 var (
-	ErrUserAlreadyExists = errors.New("user already exists")
-	ErrUserNotFound      = errors.New("user not found")
-	ErrInvalidPassword   = errors.New("invalid password")
-	ErrTimeOut           = errors.New("verification code timed out")
-	ErrCodeErrors        = errors.New("validation code errors")
-	ErrConfirmPassword   = errors.New("passwords are inconsistent")
-	ErrPassword          = errors.New("wrong password")
+	ErrUserAlreadyExists = errors.New("the user already exists, please try a different email")
+	ErrUserNotFound      = errors.New("the specified user was not found in the database")
+	ErrInvalidPassword   = errors.New("the password entered is invalid, please try again")
+	ErrTimeOut           = errors.New("the verification code has timed out, please request a new code")
+	ErrCodeErrors        = errors.New("the provided verification code is incorrect, please try again")
+	ErrConfirmPassword   = errors.New("the passwords provided do not match, please check and try again")
+	ErrPassword          = errors.New("the current password entered is incorrect, please try again")
 )
